d30b6582fc9af4dfa3b9b8db01a0530328827027
Thomas van Tilburg <thomasvt@me.com>
2018-06-01 14:56:16 +0200

    Finally done!

--- Opdracht4.py
+++ Opdracht4.py
@@ -1,7 +1,8 @@
 import math
 from lines import *
 
-class Point3D:
+
+class Point:
     def __init__(self, x=0, y=0, z=0):
         self.x, self.y, self.z = float(x), float(y), float(z)
 
@@ -12,48 +13,48 @@ class Point3D:
         sina = math.sin(rad)
         z = self.z * cosa - self.x * sina
         x = self.z * sina + self.x * cosa
-        return Point3D(x, self.y, z)
+        return Point(x, self.y, z)
 
-    def project(self, win_width, win_height, fov, viewer_distance):
+    def transform(self, win_width, win_height, fov, viewer_distance):
         """ Transforms this 3D point to 2D using a perspective projection. """
         factor = fov / (viewer_distance + self.z)
         x = self.x * factor + win_width / 2
         y = -self.y * factor + win_height / 2
-        return Point3D(x, y, 1)
+        return Point(x, y, 1)
 
 
 class Simulation:
-    def __init__(self, win_width = 640, win_height = 480):
-
-        self.vertices = [
-            Point3D(-1, 1, -1),
-            Point3D(1, 1, -1),
-            Point3D(1, -1, -1),
-            Point3D(-1, -1, -1),
-            Point3D(-1, 1, 1),
-            Point3D(1, 1, 1),
-            Point3D(1, -1, 1),
-            Point3D(-1, -1, 1)
+    def __init__(self, win_width=640, win_height=480):
+        self.points = [
+            Point(-1, -1, -1),
+            Point(1, -1, -1),
+            Point(1, 1, -1),
+            Point(-1, 1, -1),
+            Point(-1, -1, 1),
+            Point(1, -1, 1),
+            Point(1, 1, 1),
+            Point(-1, 1, 1)
         ]
 
-        # Define the vertices that compose each of the 6 faces. These numbers are
+        self.win_width = win_width
+        self.win_height = win_height
+
+        # Define the points that compose each of the 6 faces. These numbers are
         # indices to the vertices list defined above.
         self.faces = [(0, 1, 2, 3), (1, 5, 6, 2), (5, 4, 7, 6), (4, 0, 3, 7), (0, 4, 5, 1), (3, 2, 6, 7)]
 
-        self.angleX, self.angleY, self.angleZ = 0, 0, 0
-
-    def run(self):
+    def run(self, y=30):
         # Create lines class
-        l = Lines(640, 480)
+        l = Lines(self.win_width, self.win_height)
         # Will hold transformed vertices.
         t = []
 
-        for v in self.vertices:
-            # Rotate the point around X axis, then around Y axis, and finally around Z axis.
-            r = v.rotateY(30)
+        for v in self.points:
+            # Rotate the point around Y axis.
+            r = v.rotateY(y)
 
             # Transform the point from 3D to 2D
-            p = r.project(640, 480, 256, 4)
+            p = r.transform(self.win_width, self.win_height, 256, 4)
 
             # Put the point in the list of transformed vertices
             t.append(p)
@@ -67,4 +68,4 @@ class Simulation:
         l.draw()
 
 
-Simulation().run()
+Simulation().run(30)
